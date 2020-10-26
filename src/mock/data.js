455830ccf1ef23ebcba6cf0df4b59d42db7ef7f9
hojin Mun <jin1281@gmail.com>
2020-10-26 12:30:33 -0400

    update resume

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,8 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://1drv.ms/w/s!Ak-3XyGCLki9i3yEWK9N1nUnHhTJ?e=VfnfcL', // if no resume, the button will not show up
+  resume:
+    'https://onedrive.live.com/edit.aspx?resid=BD482E82215FB74F!1532&ithint=file%2cdocx&authkey=!AIRYr03WdSceFMk', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -30,7 +31,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'demo.png',
     title: 'The Gaming Network',
-    info: 'Fullstack web for video gamers find friends or other gamers to play together, team of 3 in created a full-stack web used html, css, Javascript, node.js, Auth0,socket.Io, rapid api, mongodb,jquery',
+    info:
+      'Fullstack web for video gamers find friends or other gamers to play together, team of 3 in created a full-stack web used html, css, Javascript, node.js, Auth0,socket.Io, rapid api, mongodb,jquery',
     info2: '',
     url: 'https://good-game2020.herokuapp.com/',
     repo: 'https://github.com/adbergen/the-gaming-network/', // if no repo, the button will not show up
@@ -39,7 +41,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'trace.jpg',
     title: 'Trace',
-    info: 'Web app that uses passport to authenticate and login and stores all the tracking data Team of four created tracking website that is one stop for all the carriers Technology used : HTML CSS JavaScript, Bulma, Passport, mysql',
+    info:
+      'Web app that uses passport to authenticate and login and stores all the tracking data Team of four created tracking website that is one stop for all the carriers Technology used : HTML CSS JavaScript, Bulma, Passport, mysql',
     info2: '',
     url: 'https://tracerx.herokuapp.com',
     repo: 'https://github.com/adbergen/trace', // if no repo, the button will not show up
@@ -48,7 +51,8 @@ export const projectsData = [
     id: nanoid(),
     img: 'rad.png',
     title: 'Read Your Movie',
-    info: 'Web app that allows users to search a book to see if there is a movie version with details about it and vice versa via google books API and IMDB API Technology used : Html, CSS, Javascript, ajax, jQuery, animate, google books api, imdb api',
+    info:
+      'Web app that allows users to search a book to see if there is a movie version with details about it and vice versa via google books API and IMDB API Technology used : Html, CSS, Javascript, ajax, jQuery, animate, google books api, imdb api',
     info2: '',
     url: 'https://mi6gy.github.io/read_your_movie',
     repo: 'https://github.com/mi6gy/read_your_movie', // if no repo, the button will not show up
@@ -65,7 +69,6 @@ export const contactData = {
 // FOOTER DATA
 export const footerData = {
   networks: [
-    
     {
       id: nanoid(),
       name: 'linkedin',
@@ -81,5 +84,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
